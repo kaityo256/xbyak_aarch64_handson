3a18463ec61fb147dc63c56ae4bb15a5fe54cdbe
H. Watanabe <kaityo@users.sourceforge.jp>
2021-11-18 15:22:47 +0000

    adds xbyak

---
+++ sample/xbyak/test/test.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <xbyak_aarch64/xbyak_aarch64.h>
+
+struct Code : Xbyak_aarch64::CodeGenerator{
+	Code () {
+		mov(w0,1);
+		ret();
+	}
+};
+
+int main(){
+	Code c;
+	auto f = c.getCode<int (*)()>();
+	c.ready();
+	printf("%d\n",f());
+}
