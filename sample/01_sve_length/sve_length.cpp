88eb1171766b00b1f4ba39f453c9370077d4b203
H. Watanabe <kaityo@users.sourceforge.jp>
2021-11-18 09:03:41 +0000

    adds sample01

---
+++ sample/01_sve_length/sve_length.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#ifdef __ARM_FEATURE_SVE
+#include <arm_sve.h>
+#endif
+
+int main() {
+	int n = 0;
+#ifdef __ARM_FEATURE_SVE
+	n = svcntb() * 8;
+#endif
+	if (n) {
+		printf("SVE is available. The length is %d bits\n", n);
+	} else {
+		printf("SVE is unavailable.\n");
+	}
+}
