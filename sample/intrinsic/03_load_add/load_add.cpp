3e01f65ee30a5c1e76a452156a713847bf7b99df
H. Watanabe <kaityo@users.sourceforge.jp>
2021-11-20 15:49:59 +0900

    updates

--- sample/intrinsic/03_load_add/load_add.cpp
+++ sample/intrinsic/03_load_add/load_add.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-#include <cstdio>
 #include <arm_sve.h>
+#include <cstdio>
+#include <iostream>
 #include <vector>
 
-void svshow(svfloat64_t va){
+void svshow(svfloat64_t va) {
   int n = svcntd();
   std::vector<double> a(n);
   svbool_t tp = svptrue_b64();
   svst1_f64(tp, a.data(), va);
-  for(int i=0;i<n;i++){
-    printf("%+.7f ", a[n-i-1]);
+  for (int i = 0; i < n; i++) {
+    printf("%+.7f ", a[n - i - 1]);
   }
   printf("\n");
 }
 
-void load_pat(){
+void load_pat() {
   int n = svcntd();
   std::vector<double> a(n);
-  for(int i=0;i<n;i++){
-    a[i] = (i+1);
+  for (int i = 0; i < n; i++) {
+    a[i] = (i + 1);
   }
   svbool_t tp = svptrue_b64();
   svfloat64_t va = svld1_f64(tp, a.data());
@@ -30,16 +30,16 @@ void load_pat(){
   svshow(vb);
 }
 
-double a[] = {0,1,2,3,4,5,6,7};
-double b[] = {1,1,1,1,1,1,1,1};
+double a[] = {0, 1, 2, 3, 4, 5, 6, 7};
+double b[] = {1, 1, 1, 1, 1, 1, 1, 1};
 
-void add_all(svfloat64_t va, svfloat64_t vb){
-  svfloat64_t vc = svadd_f64_z(svptrue_b64(),va,vb);
+void add_all(svfloat64_t va, svfloat64_t vb) {
+  svfloat64_t vc = svadd_f64_z(svptrue_b64(), va, vb);
   printf("va + vb (ALL) = ");
   svshow(vc);
 }
 
-int main(){
+int main() {
   svfloat64_t va = svld1_f64(svptrue_b64(), a);
   printf("va = ");
   svshow(va);
@@ -49,19 +49,19 @@ int main(){
   printf("\n");
 
   printf("ALL\n");
-  svfloat64_t vc1 = svadd_f64_z(svptrue_b64(),va,vb);
+  svfloat64_t vc1 = svadd_f64_z(svptrue_b64(), va, vb);
   printf("va + vb = ");
   svshow(vc1);
   printf("\n");
 
   printf("pattern VL2 with zero clear\n");
-  svfloat64_t vc2 = svadd_f64_z(svptrue_pat_b64(SV_VL2),va,vb);
+  svfloat64_t vc2 = svadd_f64_z(svptrue_pat_b64(SV_VL2), va, vb);
   printf("va + vb = ");
   svshow(vc2);
   printf("\n");
 
   printf("pattern VL2 with merging the first input\n");
-  svfloat64_t vc3 = svadd_f64_m(svptrue_pat_b64(SV_VL2),va,vb);
+  svfloat64_t vc3 = svadd_f64_m(svptrue_pat_b64(SV_VL2), va, vb);
   printf("va + vb = ");
   svshow(vc3);
   printf("\n");
