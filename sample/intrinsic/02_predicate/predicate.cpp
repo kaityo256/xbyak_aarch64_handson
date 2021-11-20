e13179411a28d9febff1d1cfae34c09492eef900
H. Watanabe <kaityo@users.sourceforge.jp>
2021-11-20 04:17:30 +0000

    update

--- sample/intrinsic/02_predicate/predicate.cpp
+++ sample/intrinsic/02_predicate/predicate.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 
-void show_ppr(svbool_t tp) {
+void show_pr(svbool_t tp) {
   int n = svcntb();
   std::vector<int8_t> a(n);
   std::vector<int8_t> b(n);
@@ -19,39 +19,39 @@ void show_ppr(svbool_t tp) {
 void ptrue() {
   std::cout << "# pture samples for various types" << std::endl;
   std::cout << "svptrue_b8" << std::endl;
-  show_ppr(svptrue_b8());
+  show_pr(svptrue_b8());
   std::cout << "svptrue_b16" << std::endl;
-  show_ppr(svptrue_b16());
+  show_pr(svptrue_b16());
   std::cout << "svptrue_b32" << std::endl;
-  show_ppr(svptrue_b32());
+  show_pr(svptrue_b32());
   std::cout << "svptrue_b64" << std::endl;
-  show_ppr(svptrue_b64());
+  show_pr(svptrue_b64());
 }
 
 void ptrue_pat() {
   std::cout << "# pture_pat samples for vrious patterns" << std::endl;
   std::cout << "svptrue_pat_b8(SV_ALL)" << std::endl;
-  show_ppr(svptrue_pat_b8(SV_ALL));
+  show_pr(svptrue_pat_b8(SV_ALL));
   std::cout << "svptrue_pat_b8(SV_VL1)" << std::endl;
-  show_ppr(svptrue_pat_b8(SV_VL1));
+  show_pr(svptrue_pat_b8(SV_VL1));
   std::cout << "svptrue_pat_b8(SV_VL2)" << std::endl;
-  show_ppr(svptrue_pat_b8(SV_VL2));
+  show_pr(svptrue_pat_b8(SV_VL2));
   std::cout << "svptrue_pat_b8(SV_VL3)" << std::endl;
-  show_ppr(svptrue_pat_b8(SV_VL3));
+  show_pr(svptrue_pat_b8(SV_VL3));
   std::cout << "svptrue_pat_b8(SV_VL4)" << std::endl;
-  show_ppr(svptrue_pat_b8(SV_VL4));
+  show_pr(svptrue_pat_b8(SV_VL4));
 }
 
 void ptrue_pat_types() {
   std::cout << "# pture_pat samples for various types" << std::endl;
   std::cout << "svptrue_pat_b8(SV_VL2)" << std::endl;
-  show_ppr(svptrue_pat_b8(SV_VL2));
+  show_pr(svptrue_pat_b8(SV_VL2));
   std::cout << "svptrue_pat_b16(SV_VL2)" << std::endl;
-  show_ppr(svptrue_pat_b16(SV_VL2));
+  show_pr(svptrue_pat_b16(SV_VL2));
   std::cout << "svptrue_pat_b32(SV_VL2)" << std::endl;
-  show_ppr(svptrue_pat_b32(SV_VL2));
+  show_pr(svptrue_pat_b32(SV_VL2));
   std::cout << "svptrue_pat_b64(SV_VL2)" << std::endl;
-  show_ppr(svptrue_pat_b64(SV_VL2));
+  show_pr(svptrue_pat_b64(SV_VL2));
 }
 
 int main() {
